e5113eff49b9d65a3917dcb38a39c39f0313d6ab
Agustin Marani Ghiacci <agusnicolas21@gmail.com>
2022-11-18 20:45:55 +0000

    Header & Chat v1.4

--- frontend/src/components/global/ContactMenu.js
+++ frontend/src/components/global/ContactMenu.js
@@ -1,47 +1,29 @@
-import { IconButton, Avatar, Typography, List, ListItem, Divider, Button } from "@mui/material";
+import { ListItem, Divider, Button } from "@mui/material";
 import { Stack } from "@mui/system";
 import React from "react";
 import { useState } from "react";
 import AddOutlinedIcon from '@mui/icons-material/AddOutlined';
 import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
 import ChatList from "./ChatList";
-import Skeleton from '@mui/material/Skeleton';
-import SwipeableDrawer from '@mui/material/SwipeableDrawer';
 
 export default function ContactMenu() {
-    const drawerBleeding = 556;
     const [expand, setExpand] = useState(false);
-    const [open, setOpen] = useState(false);
 
-    const toggleDrawer = (newOpen) => () => {
-        setOpen(newOpen);
-    };
     function handleClick() {
         setExpand(!expand);
     }
 
     return (
         <>
-            <Stack className={expand ? "menu-controller expanded" : "menu-controller"} justifyContent="center">
-                <Button color="blue" className="menu-controller-btn" variant="contained" component="label" onClick={toggleDrawer(true)}>
+            <Stack className={expand ? "menu-controller expanded-menu" : "menu-controller"} justifyContent="center">
+                <Button color="blue" className="menu-controller-btn" variant="contained" component="label" onClick={handleClick}>
                     {expand ? <CloseOutlinedIcon></CloseOutlinedIcon> : <AddOutlinedIcon></AddOutlinedIcon>}
                 </Button>
+                <Stack className={expand ? "expanded-list" : "contact-list"}>
+                    <ChatList></ChatList>
+                </Stack>
             </Stack >
-            <SwipeableDrawer
-                // container={container}
-                anchor="bottom"
-                open={open}
-                onClose={toggleDrawer(false)}
-                onOpen={toggleDrawer(true)}
-                swipeAreaWidth={drawerBleeding}
-                disableSwipeToOpen={false}
-                ModalProps={{
-                    keepMounted: true,
-                }}
-            >
-                <ChatList></ChatList>
-                <Skeleton variant="rectangular" height="100%" />
-            </SwipeableDrawer>
+
         </>
 
 
