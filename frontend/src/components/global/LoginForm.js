ec797dcc8ad032daac6576545f66e0574f192491
Agustin Marani Ghiacci <agusnicolas21@gmail.com>
2022-12-05 20:44:10 +0000

    User Registration Connection

--- frontend/src/components/global/LoginForm.js
+++ frontend/src/components/global/LoginForm.js
@@ -1,40 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import { FormControl, FormHelperText, Input, InputLabel, Button } from "@mui/material";
-import { Container, Stack } from "@mui/system";
+import { Box, Stack } from "@mui/system";
 import Typography from '@mui/material/Typography';
+import axios from 'axios'
 
 export default function LoginForm() {
+
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = event => {
+    setUser({
+      ...user,
+      [event.target.name]: event.target.value
+    })
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    axios.post(`http://127.0.0.1:3000/sessions`, { user })
+      .then(console.log("ok"))
+  };
+
   return (
     <>
-      <Container>
+      <Box component="form" onSubmit={handleSubmit}>
         <Stack spacing={3} justifyContent="center" alignItems="center">
-          <item>
-            <FormControl variant="standard" color="blue" >
-              <InputLabel htmlFor="email" >Username</InputLabel>
-              <Input id="email" type="email" />
-            </FormControl>
-          </item>
-          <item>
-            <FormControl variant="standard" color="blue">
-              <InputLabel htmlFor="pwd">Password</InputLabel>
-              <Input id="pwd" type="password" />
-            </FormControl>
-          </item>
-          <item>
-            <Button className="logIn-btn" variant="contained" color="blue" size="large">Log In</Button>
-          </item>
+          <FormControl variant="standard" color="blue" >
+            <InputLabel htmlFor="email" >Username</InputLabel>
+            <Input id="email" type="email" onChange={handleChange} value={user.email} />
+          </FormControl>
+          <FormControl variant="standard" color="blue">
+            <InputLabel htmlFor="password">Password</InputLabel>
+            <Input id="password" type="password" onChange={handleChange} value={user.password} />
+          </FormControl>
+          <Button className="logIn-btn" variant="contained" color="blue" size="large" type="submit">Log In</Button>
         </Stack>
         <Stack direction="row" justifyContent="center" alignItems="center" mt={1}>
-          <item>
-            <Typography className="test" variant="body2" color="white">
-              No account yet?
-            </Typography>
-          </item>
-          <item>
-            <Button sx={{ verticalAlign: "baseline" }} className="test2" variant="text" color="blue">Sign Up</Button>
-          </item>
+          <Typography className="test" variant="body2" color="white">
+            No account yet?
+          </Typography>
+          <Button sx={{ verticalAlign: "baseline" }} className="test2" variant="text" color="blue">Sign Up</Button>
         </Stack>
-      </Container>
+      </Box>
     </>
   )
 }
--- frontend/src/components/global/SignInForm.js
+++ frontend/src/components/global/SignInForm.js
@@ -1,45 +1,71 @@
-import React from "react";
-import { FormControl, FormHelperText, Input, InputLabel, Button } from "@mui/material";
+import React, { useState } from "react";
+import { FormControl, Input, InputLabel, Button, Box } from "@mui/material";
 import { Stack } from "@mui/system";
 import Typography from '@mui/material/Typography';
 import AddPhotoAlternateIcon from '@mui/icons-material/AddPhotoAlternate';
+import axios from 'axios'
 
 export default function SignInForm() {
+
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+    username: "",
+    password_confirmation: "",
+    status: "active"
+  });
+
+  const handleChange = event => {
+    setUser({
+      ...user,
+      [event.target.name]: event.target.value
+    })
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    axios.post(`http://127.0.0.1:3000/users`, { user })
+      .then(console.log("ok"))
+  };
+
   return (
     <>
-      <Stack direction="row" justifyContent="center" alignItems="center" spacing={4} sx={{ padding: "0 1em", margin: "1em 0", height: "40vh" }}>
+      <Box component="form" onSubmit={handleSubmit}>
+        <Stack direction="row" justifyContent="center" alignItems="center" spacing={4} sx={{ padding: "0 1em", margin: "1em 0", height: "40vh" }}>
+          <Stack>
+            <Button variant="outlined" component="label" color="blue" sx={{ borderRadius: "50%", padding: "2em", minWidth: "inherit", width: "10em", height: "10em" }} >
+              <Stack justifyContent="center" alignItems="center" sx={{ '& .MuiStack-root': { alignItems: "center" }, rowGap: "1em" }}>
+                <AddPhotoAlternateIcon fontSize="large" />
+                <Typography variant="subtitle" align="center">
+                  Image Profile
+                </Typography>
+              </Stack>
+              <input hidden accept="image/*" multiple type="file" />
+            </Button>
+          </Stack>
+          <Stack spacing={2.5} sx={{ paddingBottom: "1em" }}>
+            <FormControl variant="standard" color="blue" >
+              <InputLabel htmlFor="username" >Username</InputLabel>
+              <Input id="username" name="username" type="text" onChange={handleChange} value={user.username} />
+            </FormControl>
+            <FormControl variant="standard" color="blue">
+              <InputLabel htmlFor="email">Email</InputLabel>
+              <Input id="email" name="email" type="email" onChange={handleChange} value={user.email} />
+            </FormControl>
+            <FormControl variant="standard" color="blue">
+              <InputLabel htmlFor="password">Password</InputLabel>
+              <Input id="password" name="password" type="password" onChange={handleChange} value={user.password} />
+            </FormControl>
+            <FormControl variant="standard" color="blue">
+              <InputLabel htmlFor="password_confirmation">Repeat Password</InputLabel>
+              <Input id="password_confirmation" name="password_confirmation" type="password" onChange={handleChange} value={user.password_confirmation} />
+            </FormControl>
+          </Stack>
+        </Stack>
         <Stack>
-          <Button variant="outlined" component="label" color="blue" sx={{ borderRadius: "50%", padding: "2em", minWidth: "inherit", width: "10em", height: "10em" }} >
-            <Stack justifyContent="center" alignItems="center" sx={{ '& .MuiStack-root': { alignItems: "center" }, rowGap: "1em" }}>
-              <AddPhotoAlternateIcon fontSize="large" />
-              <Typography variant="subtitle" align="center">
-                Image Profile
-              </Typography>
-            </Stack>
-            <input hidden accept="image/*" multiple type="file" />
-          </Button>
-
+          <Button sx={{ margin: "0 auto" }} variant="contained" color="blue" size="large" type="submit">Sign In</Button>
         </Stack>
-        <Stack spacing={2.5} sx={{ paddingBottom: "1em" }}>
-          <FormControl variant="standard" color="blue" >
-            <InputLabel htmlFor="userName" >Username</InputLabel>
-            <Input id="userName" type="text" />
-          </FormControl>
-          <FormControl variant="standard" color="blue">
-            <InputLabel htmlFor="email">Email</InputLabel>
-            <Input id="email" type="password" />
-          </FormControl>
-          <FormControl variant="standard" color="blue">
-            <InputLabel htmlFor="pwd">Password</InputLabel>
-            <Input id="pwd" type="password" />
-          </FormControl>
-          <FormControl variant="standard" color="blue">
-            <InputLabel htmlFor="pwd-cf">Repeat Password</InputLabel>
-            <Input id="pwd-cf" type="password" />
-          </FormControl>
-        </Stack>
-      </Stack>
-      <Button sx={{ margin: "0 auto" }} variant="contained" color="blue" size="large">Sign In</Button>
+      </Box>
     </>
   )
 }
