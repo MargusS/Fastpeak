a3451a41395a44707141cb9f30fb8de6d812c120
MargusS <agusnicolas21@gmail.com>
2022-12-14 22:14:13 +0000

    Delete Chat

--- API/app/controllers/chats_controller.rb
+++ API/app/controllers/chats_controller.rb
@@ -7,9 +7,9 @@ class ChatsController < ApplicationController
     # create a new member with the current user
     @member = Member.create(user_id: params['chat']['user'], chat_id: @chat.id)
     # create the member with the members list
-    @member = Member.create(user_id:@current_user.id, chat_id:@chat.id)
+    @member = Member.create(user_id: 1, chat_id:@chat.id)
     # create a new message
-    @message = Message.create(content: params['chat']['message']['content'], user_id: @current_user.id, chat_id: @chat.id)
+    @message = Message.create(content: params['chat']['message'], user_id: 1, chat_id: @chat.id)
 
     render json: @chat
   end
--- API/app/models/chat.rb
+++ API/app/models/chat.rb
@@ -1,5 +1,5 @@
 class Chat < ApplicationRecord
-  has_many :messages
-  has_many :members
+  has_many :messages, dependent: :delete_all
+  has_many :members, dependent: :delete_all
   has_many :users, through: :members
 end
