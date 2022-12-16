8b5c60e18293bc9eecd3ae7dee1421209751fde6
Agustin Marani Ghiacci <agusnicolas21@gmail.com>
2022-12-16 22:41:25 +0000

    Websocket v1.4

--- API/app/controllers/application_controller.rb
+++ API/app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
     include CurrentUserConcern
-    # before_action :logged_in
+    before_action :logged_in
 
   def index
       @all = controller_name.classify.constantize.all 
@@ -31,11 +31,11 @@ class ApplicationController < ActionController::API
       render json: @all
   end 
 
-#   def logged_in
-#     unless @current_user
-#         render json: {
-#             logged_in: false
-#           }, status: :unauthorized and return
-#     end
-#   end
+  def logged_in
+    unless @current_user
+        render json: {
+            logged_in: false
+          }, status: :unauthorized and return
+    end
+  end
 end
