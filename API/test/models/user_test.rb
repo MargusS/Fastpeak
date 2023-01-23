41d61a4b6bf90b1a4c9cc5b599e9223d1b7a6bf4
Agustin Marani Ghiacci <agusnicolas21@gmail.com>
2023-01-23 16:03:49 +0000

    User data test v1.1

--- API/test/models/user_test.rb
+++ API/test/models/user_test.rb
@@ -15,6 +15,10 @@ class UserTest < ActiveSupport::TestCase
     @user.username = 'a' * 25
     assert_not @user.valid?, "Name should be at most 25 characters long"
   end
+  test "name should be ok with 15 chars" do
+    @user.username = 'a' * 15
+    assert_not @user.valid?, "name should be ok with 15 chars"
+  end
 
   test "email should be valid" do
     @user.email = 'invalid_email'
