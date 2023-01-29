46fa1704fbc8b11e6d4ab64dc5441259731d66a4
Agustin Marani Ghiacci <agusnicolas21@gmail.com>
2023-01-29 22:26:52 +0000

    BAckend Test Changes

--- API/app/models/user.rb
+++ API/app/models/user.rb
@@ -10,8 +10,12 @@ class User < ApplicationRecord
   has_many :chats, through: :members
 
   validates :username, length: { minimum: 2, maximum: 25 }
+
+  PASSWORD_FORMAT = /\A(?=.{8,})/x
+
   has_secure_password 
-  validates :password, presence: true, length: { minimum: 8 }
+  validates :password, presence: true,length: {minimum: 8}, format: { with: PASSWORD_FORMAT }, confirmation: true, on: :create 
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
 
   def avatar_url
     Rails.application.routes.url_helpers.url_for(avatar) if avatar.attached?
--- API/test/models/user_test.rb
+++ API/test/models/user_test.rb
@@ -13,17 +13,28 @@ class UserTest < ActiveSupport::TestCase
 
   test "name should be at most 25 characters long" do
     @user.username = 'a' * 25
-    assert_not @user.valid?, "Name should be at most 25 characters long"
+    assert @user.valid?, "Name should be at most 25 characters long"
+  end
+  test "name should be ok at 15 chars" do
+    @user.username = 'aaa' 
+    assert @user.valid?, "name should be ok at 15 chars"
   end
 
   test "email should be valid" do
-    @user.email = 'invalid_email'
-    assert_not @user.valid?, "Email should follow the standard email structure"
+    @user.email = 'agus@agus.com'
+    assert @user.valid?, "Email should follow the standard email structure"
   end
 
-  test "password should be at most 8 characters long" do
-    @user.password_digest = 'pwd'
+  test "password should be at least 8 characters long" do
+    @user = User.new(email:"agus@user.com", username:"test")
+    @user.password = "123"
     assert_not @user.valid?, "Password should be at least 8 characters long"
+  end
+
+  test "password should be at least 8 characters long2" do
+    @user = User.new(email:"agus@user.com", username:"test")
+    @user.password = "123456789"
+    assert @user.valid?, "Password should be at least 8 characters long"
   end
 
 end
