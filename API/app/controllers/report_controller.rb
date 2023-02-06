e4a98821c1ff631847b89620d29747a29bfca442
Agustin Marani Ghiacci <agusnicolas21@gmail.com>
2023-02-06 00:26:06 +0000

    PDF generator v1.1

--- API/app/controllers/report_controller.rb
+++ API/app/controllers/report_controller.rb
@@ -4,12 +4,10 @@ class ReportController < ApplicationController
   skip_before_action :logged_in
   require "prawn"
 
-  def create_report
-    pdf=Prawn::Document.new
-    pdf.text "Hello World"
-    send_data(
-      pdf.render,
-      filename: 'hello.pdf',
-      type: 'application/pdf')
+  def show
+    pdf = WickedPdf.new.pdf_from_string(
+      render_to_string(layout: 'layouts/show')
+    )
+    send_data pdf, filename: "report.pdf"
   end 
 end
