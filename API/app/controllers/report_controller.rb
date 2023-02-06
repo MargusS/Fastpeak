eb4509234ceee57f8d363fbb5b8884013718818f
Agustin Marani Ghiacci <agusnicolas21@gmail.com>
2023-02-06 20:35:20 +0000

    Report Data

--- API/app/controllers/report_controller.rb
+++ API/app/controllers/report_controller.rb
@@ -1,10 +1,13 @@
 class ReportController < ApplicationController
   include CurrentUserConcern
-  include ActionController::MimeResponds
   skip_before_action :logged_in
-  require "prawn"
 
   def show
+    @current_month = Date.today.strftime("%m").to_i
+    @total_messages_sent = Message.where(user_id: 1).count
+    @month_messages_sent = Message.where(user_id: 1).group('(EXTRACT(MONTH FROM created_at))::integer').count
+    @total_messages_rec = Chat.joins(:members).where(:members => {user_id: 1}).joins(:messages).where.not(:messages => {user_id: 1}).count
+    @month_messages_rec = Chat.joins(:members).where(:members => {user_id: 1}).joins(:messages).where.not(:messages => {user_id: 1}).group('(EXTRACT(MONTH FROM messages.created_at))::integer').count
     pdf = WickedPdf.new.pdf_from_string(
       render_to_string(layout: 'layouts/show')
     )
